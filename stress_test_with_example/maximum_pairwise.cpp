d1f49957406a94b6419d0f2091de514e50f89271
Mohamed Helal <mas_sina@hotmail.com>
2020-07-03 14:37:42 +0300

    update maximum_pairwise to support long integers

--- stress_test_with_example/maximum_pairwise.cpp
+++ stress_test_with_example/maximum_pairwise.cpp
@@ -5,20 +5,35 @@ using std::cin;
 using std::cout;
 using std::vector;
 
-int MaxPairWiseProduct(const vector<int> &numbers)
+long long MaxPairWiseProduct(const vector<int> &numbers)
 {
-    int result = 0;
+    long long result = 0;
     int n = numbers.size();
-    for (int i = 0; i < n; ++i)
+    for (int i = 0; i < n; i++)
     {
         for (int j = i + 1; j < n; j++)
         {
-            if(numbers[i] * numbers[j] > result)
+            if ((long long)numbers[i] * numbers[j] > result)
             {
-                result = numbers[i] * numbers[j];
+                result = (long long)numbers[i] * numbers[j];
             }
         }
     }
 
     return result;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> numbers(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> numbers[i];
+    }
+
+    long long result = MaxPairWiseProduct(numbers);
+    cout << result << "\n";
+    return 0;
 }
