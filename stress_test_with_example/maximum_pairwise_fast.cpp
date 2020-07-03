ee4f4e8b1d97b09c0e04f91ddeea30ce34d4f6c1
Mohamed Helal <mas_sina@hotmail.com>
2020-07-03 15:46:19 +0300

    add maximum pairwise product fast

--- stress_test_with_example/maximum_pairwise_fast.cpp
+++ stress_test_with_example/maximum_pairwise_fast.cpp
@@ -5,24 +5,6 @@ using std::cin;
 using std::cout;
 using std::vector;
 
-long long MaxPairWiseProduct(const vector<int> &numbers)
-{
-    long long result = 0;
-    int n = numbers.size();
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i + 1; j < n; j++)
-        {
-            if ((long long)numbers[i] * numbers[j] > result)
-            {
-                result = (long long)numbers[i] * numbers[j];
-            }
-        }
-    }
-
-    return result;
-}
-
 long long MaxPairwiseProductFast(vector<int> &numbers)
 {
     int n = numbers.size();
@@ -39,7 +21,7 @@ long long MaxPairwiseProductFast(vector<int> &numbers)
 
     for (int j = 0; j < n; j++)
     {
-        if (max_index2 != max_index1 && numbers[j] > numbers[max_index2])
+        if (j != max_index1 && numbers[j] > numbers[max_index2])
         {
             max_index2 = j;
         }
@@ -58,7 +40,7 @@ int main()
         cin >> numbers[i];
     }
 
-    long long result = MaxPairWiseProduct(numbers);
+    long long result = MaxPairwiseProductFast(numbers);
     cout << result << "\n";
     return 0;
 }
