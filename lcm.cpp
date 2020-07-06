feb57f0a79665b3b7fd052d789d83564eaaaaa06
Mohammad Helal <mhelal@dttmoj.onmicrosoft.com>
2020-07-07 00:04:16 +0300

    add LCM

---
+++ lcm.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cassert>
+#include <cstdlib>
+
+using namespace std;
+
+long long lcm_naive(int a, int b)
+{
+  for (long l = 1; l <= (long long)a * b; ++l)
+    if (l % a == 0 && l % b == 0)
+      return l;
+
+  return (long long)a * b;
+}
+
+int gcd_fast(int a, int b)
+{
+  if (b == 0)
+    return a;
+
+  return gcd_fast(b, a % b);
+}
+
+long long lcm_fast(int a, int b)
+{
+  return ((long long)a * b) / gcd_fast(a, b);
+}
+
+void test_solution()
+{
+  assert(lcm_naive(6, 8) == 24);
+  assert(lcm_fast(6, 8) == 24);
+
+  while (true)
+  {
+    int a = rand() + 1 % 100000;
+    int b = rand() + 1 % 100000;
+
+    cout << "a: " << a << " b: " << b << endl;
+
+    int res1 = lcm_naive(a, b);
+    int res2 = lcm_fast(a, b);
+
+    if (res1 != res2)
+    {
+      cout << "Wrong: " << res1 << " " << res2 << endl;
+      return;
+    }
+    else
+    {
+      cout << "OK" << endl;
+    }
+  }
+}
+
+int main()
+{
+  int a, b;
+  cin >> a >> b;
+  // cout << lcm_naive(a, b) << endl;
+  // test_solution();
+  cout << lcm_naive(a, b) << endl;
+  return 0;
+}
